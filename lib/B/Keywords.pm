dc47c1071ea3603fa0311ab3294f936a6a32a047
Joshua ben Jore <jbenjore@whitepages.com>
2009-03-08 20:19:52 -0700

    B-Keywords-0.03

--- lib/B/Keywords.pm
+++ lib/B/Keywords.pm
@@ -1,31 +1,32 @@
 package B::Keywords;
+use strict;
+use warnings FATAL => qw(all);
+use vars qw(
+    $VERSION @Scalars @Arrays @Hashes @Filehandles @Symbols @Functions
+    @Barewords);
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 
-@Symbols = qw[
+@Scalars = (qw[
  $a
  $b
- %OVERLOAD
- %!
  $_
  $& $MATCH
  $` $PREMATCH
  $' $POSTMATCH
  $+ $LAST_PAREN_MATCH
- @+ $LAST_MATCH_END
  $* $MULTILINE_MATCHING
  $. $INPUT_LINE_NUMBER $NR
  $/ $INPUT_RECORD_SEPARATOR $RS
  $| $OUTPUT_AUTO_FLUSH
- $, $OUTPUT_FIELD_SEPARATOR $OFS
+ ], '$,', qw[$OUTPUT_FIELD_SEPARATOR $OFS
  $\ $OUTPUT_RECORD_SEPARATOR $ORS
  $" $LIST_SEPARATOR
  $; $SUBSCRIPT_SEPARATOR $SUBSEP
- $# $OFMT
+ ], '$#', qw[$OFMT
  $% $FORMAT_PAGE_NUMBER
  $= $FORMAT_LINES_PER_PAGE
  $- $FORMAT_LINES_LEFT
- @- @LAST_MATCH_START
  $~ $FORMAT_NAME
  $^ $FORMAT_TOP_NAME
  $: $FORMAT_LINE_BREAK_CHARACTERS
@@ -47,7 +48,6 @@ $VERSION = '0.02';
  $^D $DEBUGGING
  $^F $SYSTEM_FD_MAX
  $^H
- %^H
  $^I $INPLACE_EDIT
  $^M
  $^O $OSNAME
@@ -60,266 +60,282 @@ $VERSION = '0.02';
  ${^WARNING_BITS}
  ${^WIDE_SYSTEM_CALLS}
  $^X $EXECUTABLE_NAME
- $ARGV
+ $ARGV]);
+
+@Arrays = qw[
+ @+ $LAST_MATCH_END
+ @- @LAST_MATCH_START
  @ARGV
  @INC
- @_
+ @_];
+
+@Hashes = qw[
+ %OVERLOAD
+ %!
+ %^H
  %INC
  %ENV
- %SIG
+ %SIG];
+
+@Filehandles = qw[
  *ARGV ARGV
  ARGVOUT
  STDIN
  STDOUT
  STDERR];
 
-@Barewords = qw[
+@Symbols = (@Scalars, @Arrays, @Hashes, @Filehandles);
+
+@Functions = qw[
+ AUTOLOAD
+ BEGIN
+ DESTROY
+ END
+ INIT
+ CHECK
+ abs
+ accept
+ alarm
+ and
+ atan2
+ bind
+ binmode
+ bless
+ caller
+ chdir
+ chmod
+ chomp
+ chop
+ chown
+ chr
+ chroot
+ close
+ closedir
+ cmp
+ connect
+ continue
+ cos
+ crypt
+ dbmclose
+ dbmopen
+ defined
+ delete
+ die
+ do
+ dump
+ each
+ else
+ elsif
+ endgrent
+ endhostent
+ endnetent
+ endprotoent
+ endpwent
+ endservent
+ eof
+ eq
+ eval
+ exec
+ exists
+ exit
+ exp
+ fcntl
+ fileno
+ flock
+ for
+ foreach
+ fork
+ format
+ formline
+ ge
+ getc
+ getgrent
+ getgrgid
+ getgrnam
+ gethostbyaddr
+ gethostbyname
+ gethostent
+ getlogin
+ getnetbyaddr
+ getnetbyname
+ getnetent
+ getpeername
+ getpgrp
+ getppid
+ getpriority
+ getprotobyname
+ getprotobynumber
+ getprotoent
+ getpwent
+ getpwnam
+ getpwuid
+ getservbyname
+ getservbyport
+ getservent
+ getsockname
+ getsockopt
+ glob
+ gmtime
+ goto
+ grep
+ gt
+ hex
+ if
+ index
+ int
+ ioctl
+ join
+ keys
+ kill
+ last
+ lc
+ lcfirst
+ le
+ length
+ link
+ listen
+ local
+ localtime
+ log
+ lstat
+ lt
+ m
+ map
+ mkdir
+ msgctl
+ msgget
+ msgrcv
+ msgsnd
+ my
+ ne
+ next
+ no
+ not
+ oct
+ open
+ opendir
+ or
+ ord
+ our
+ pack
+ package
+ pipe
+ pop
+ pos
+ print
+ printf
+ prototype
+ push
+ q
+ qq
+ qr
+ quotemeta
+ qw
+ qx
+ rand
+ read
+ readdir
+ readline
+ readlink
+ readpipe
+ recv
+ redo
+ ref
+ rename
+ require
+ reset
+ return
+ reverse
+ rewinddir
+ rindex
+ rmdir
+ s
+ scalar
+ seek
+ seekdir
+ select
+ semctl
+ semget
+ semop
+ send
+ setgrent
+ sethostent
+ setnetent
+ setpgrp
+ setpriority
+ setprotoent
+ setpwent
+ setservent
+ setsockopt
+ shift
+ shmctl
+ shmget
+ shmread
+ shmwrite
+ shutdown
+ sin
+ sleep
+ socket
+ socketpair
+ sort
+ splice
+ split
+ sprintf
+ sqrt
+ srand
+ stat
+ study
+ sub
+ substr
+ symlink
+ syscall
+ sysopen
+ sysread
+ sysseek
+ system
+ syswrite
+ tell
+ telldir
+ tie
+ tied
+ time
+ times
+ tr
+ truncate
+ uc
+ ucfirst
+ umask
+ undef
+ unless
+ unlink
+ unpack
+ unshift
+ untie
+ until
+ use
+ utime
+ values
+ vec
+ wait
+ waitpid
+ wantarray
+ warn
+ while
+ write
+ x
+ xor
+ y];
+
+@Barewords = (qw[
  NULL
  __FILE__
  __LINE__
  __PACKAGE__
  __DATA__
  __END__
-AUTOLOAD
-BEGIN
-CORE
-DESTROY
-END
-INIT
-CHECK
-abs
-accept
-alarm
-and
-atan2
-bind
-binmode
-bless
-caller
-chdir
-chmod
-chomp
-chop
-chown
-chr
-chroot
-close
-closedir
-cmp
-connect
-continue
-cos
-crypt
-dbmclose
-dbmopen
-defined
-delete
-die
-do
-dump
-each
-else
-elsif
-endgrent
-endhostent
-endnetent
-endprotoent
-endpwent
-endservent
-eof
-eq
-eval
-exec
-exists
-exit
-exp
-fcntl
-fileno
-flock
-for
-foreach
-fork
-format
-formline
-ge
-getc
-getgrent
-getgrgid
-getgrnam
-gethostbyaddr
-gethostbyname
-gethostent
-getlogin
-getnetbyaddr
-getnetbyname
-getnetent
-getpeername
-getpgrp
-getppid
-getpriority
-getprotobyname
-getprotobynumber
-getprotoent
-getpwent
-getpwnam
-getpwuid
-getservbyname
-getservbyport
-getservent
-getsockname
-getsockopt
-glob
-gmtime
-goto
-grep
-gt
-hex
-if
-index
-int
-ioctl
-join
-keys
-kill
-last
-lc
-lcfirst
-le
-length
-link
-listen
-local
-localtime
-lock
-log
-lstat
-lt
-m
-map
-mkdir
-msgctl
-msgget
-msgrcv
-msgsnd
-my
-ne
-next
-no
-not
-oct
-open
-opendir
-or
-ord
-our
-pack
-package
-pipe
-pop
-pos
-print
-printf
-prototype
-push
-q
-qq
-qr
-quotemeta
-qw
-qx
-rand
-read
-readdir
-readline
-readlink
-readpipe
-recv
-redo
-ref
-rename
-require
-reset
-return
-reverse
-rewinddir
-rindex
-rmdir
-s
-scalar
-seek
-seekdir
-select
-semctl
-semget
-semop
-send
-setgrent
-sethostent
-setnetent
-setpgrp
-setpriority
-setprotoent
-setpwent
-setservent
-setsockopt
-shift
-shmctl
-shmget
-shmread
-shmwrite
-shutdown
-sin
-sleep
-socket
-socketpair
-sort
-splice
-split
-sprintf
-sqrt
-srand
-stat
-study
-sub
-substr
-symlink
-syscall
-sysopen
-sysread
-sysseek
-system
-syswrite
-tell
-telldir
-tie
-tied
-time
-times
-tr
-truncate
-uc
-ucfirst
-umask
-undef
-unless
-unlink
-unpack
-unshift
-untie
-until
-use
-utime
-values
-vec
-wait
-waitpid
-wantarray
-warn
-while
-write
-x
-xor
-y];
+ CORE
+ lock],
+ @Functions);
 
 1;
 
@@ -337,17 +353,17 @@ B::Keywords - Lists of reserved barewords and symbol names
 
 =head1 DESCRIPTION
 
-B::Keywords supplies the two arrays - @Symbols and @Barewords. The barewords
-list is copied right from the source file keywords.pl with the addition of
-'lock'. @Symbols was created by reading L<perlvar> and the delta
-documentation. All additions and modifications are welcome.
+B::Keywords supplies seven arrays of keywords: @Scalars, @Arrays, @Hashes,
+@Filehandles, @Symbols, @Functions and @Barewords. The @Symbols array includes
+the contents of each of @Scalars, @Arrays, @Hashes and @Filehandles.
+Similarly, @Barewords adds a few non-function keywords (like __DATA__, NULL)
+to the @Functions array.
+
+All additions and modifications are welcome.
 
 =head1 SEE ALSO
 
-keywords.pl from the perl source
-L<perlvar>
-L<perlfunc>
-L<perldelta>
+keywords.pl from the perl source, L<perlvar>, L<perlfunc>, L<perldelta>
 
 =head1 AUTHOR
 
